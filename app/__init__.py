4430d9d8d0a6a9ade5e5ddd7200bfbea1d4bba51
lucasSperafico <sperafico.lucas.s@gmail.com>
2025-09-08 21:41:09 -0300

    commit de lista e uso de url para mecher em banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from .hello.routes import hello_bp
-from .hello.routes import name_bp
+from .hello.routes import hello_bp, name_bp 
 from .models import db
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello.db'
-
     db.init_app(app)    
     with app.app_context():
         db.create_all()
@@ -15,6 +13,5 @@ def create_app():
 
     app.register_blueprint(hello_bp)
     app.register_blueprint(name_bp)
-
 
     return app
--- app/models.py
+++ app/models.py
@@ -4,4 +4,4 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique = True, nullable = False)
